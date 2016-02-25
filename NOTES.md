1389bc4d395a968c6800378bed95b3c9ae529912
Andrew Liebchen <andrewliebchen@gmail.com>
2016-02-24 22:12:49 -0800

    Moar work

--- NOTES.md
+++ NOTES.md
@@ -3,5 +3,8 @@ Specify when you wake up, go to bed.
 Enter medication and frequency.
 * name of med
 * frequency
+  * How many times a day?
+  * OR what hours?
+  * OR Wake/bed?
 
 Prompt to take med, respond with how you feel (maybe via email?)
--- components/home.jsx
+++ components/home.jsx
@@ -9,65 +9,89 @@ Home = React.createClass({
     };
   },
 
+  getInitialState() {
+    return {
+      wakeTime: 9,
+      bedTime: 9,
+      dayLength: 12
+    };
+  },
+
   handleSaveDayLength() {
     const wakeTime = parseInt(this.refs.wake.value);
     const bedTime = parseInt(this.refs.sleep.value);
-    console.log((bedTime + 12) - wakeTime); // length of day
+    const dayLength = (bedTime + 12) - wakeTime;
+    this.setState({
+      wakeTime: wakeTime,
+      bedTime: bedTime,
+      dayLength: dayLength
+    });
   },
 
   render() {
     const {medications} = this.data;
+    const {wakeTime, bedTime, dayLength} = this.state;
     return (
       <div className="wrapper">
-        <div className="form-group">
-          <label>What time do you usually wake up?</label>
-          <div className="input-group">
-            <input type="number" min="1" max="12" className="form-control" ref="wake"/>
-            <div className="input-group-addon">am</div>
+        <section className="panel panel-default">
+          <div className="panel-body">
+            <div className="form-group">
+              <label>What time do you usually wake up?</label>
+              <div className="input-group">
+                <input type="number" min="1" max="12" className="form-control" ref="wake"/>
+                <div className="input-group-addon">am</div>
+              </div>
+            </div>
+            <div className="form-group">
+              <label>What time do you usually go to bed?</label>
+              <div className="input-group">
+                <input type="number" min="1" max="12" className="form-control" ref="sleep"/>
+                <div className="input-group-addon">pm</div>
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={this.handleSaveDayLength}>
+              Save
+            </button>
           </div>
-        </div>
-        <div className="form-group">
-          <label>What time do you usually go to bed?</label>
-          <div className="input-group">
-            <input type="number" min="1" max="12" className="form-control" ref="sleep"/>
-            <div className="input-group-addon">pm</div>
-          </div>
-        </div>
-        <button
-          type="submit"
-          className="btn btn-primary"
-          onClick={this.handleSaveDayLength}>
-          Save
-        </button>
+        </section>
 
-        {medications.map((medication, i) => {
-          return (
-            <table className="table" key={i}>
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  {_.times(24, (i) => {
-                    return <th key={i}>{i}</th>;
-                  })}
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{medication.name}</td>
-                  {_.times(24, (i) => {
-                    console.log(_.indexOf(medication.dose, i));
-                    return (
-                      <td key={i}>
-                        {_.indexOf(medication.dose, i) === -1 ? '' :  '💊'}
-                      </td>
-                    );
-                  })}
-                </tr>
-              </tbody>
-            </table>
-          );
-        })}
+        <section className="panel panel-default">
+          <header className="panel-heading">
+            <h3 className="panel-title">Schedule</h3>
+          </header>
+          {medications.map((medication, i) => {
+            return (
+              <table className="table" key={i}>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    {_.times(dayLength, (i) => {
+                      return <th key={i}>{wakeTime + i}</th>;
+                    })}
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{medication.name}</td>
+                    {_.times(dayLength, (i) => {
+                      return (
+                        <td key={i}>
+                          {_.indexOf(medication.dose, i) === -1 ? '' :  '💊'}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                </tbody>
+              </table>
+            );
+          })}
+          <div className="panel-body">
+            Add medication
+          </div>
+        </section>
       </div>
     );
   }
